6f0dd1add28d23a89261cab244618ea86f902e6c
Local Piper <artsor53@gmail.com>
2024-03-08 20:47:46 +0300

    feat: added rotation

--- scenes/game/player/Player.gd
+++ scenes/game/player/Player.gd
@@ -1,25 +1,32 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+
 const JUMP_VELOCITY = -400.0
+const JUMP_TIME_INTERVAL = 500
+const MAX_DEGREE_TURN = 90 
+const MIN_DEGREE_TURN = -30
+const ROTATION_SPEED = 2 
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var jump_time = 0
+var time_now = 0
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		time_now = Time.get_ticks_msec()
+		if time_now - jump_time >= JUMP_TIME_INTERVAL:
+			if rotation_degrees < MAX_DEGREE_TURN:
+				rotation_degrees += ROTATION_SPEED
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+		rotation_degrees = MIN_DEGREE_TURN
+		jump_time = Time.get_ticks_msec()
 
 	move_and_slide()
 
